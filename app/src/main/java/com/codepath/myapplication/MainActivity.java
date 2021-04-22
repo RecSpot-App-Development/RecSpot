635b8d79cf412267ee6c682dbbb4eff48a58f553
HAFSAnCR7 <hafsanadim2@yahoo.com>
2021-04-22 17:36:17 -0400

    adding and creating

--- app/src/main/java/com/codepath/myapplication/MainActivity.java
+++ app/src/main/java/com/codepath/myapplication/MainActivity.java
@@ -1,13 +1,75 @@
 package com.codepath.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button record_btn;
+    Button music_btn;
+    Button edit_btn;
+    Button save_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        record_btn = findViewById(R.id.record_btn);
+        music_btn = findViewById(R.id.music_btn);
+        edit_btn = findViewById(R.id.edit_btn);
+        save_btn = findViewById(R.id.save_btn);
+
+        save_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSaveActivity();
+            }
+        });
+
+
+        edit_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openEditActivity();
+            }
+        });
+
+        record_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openVideoActivity();
+            }
+        });
+
+        music_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMusicActivity();
+            }
+        });
+    }
+
+    private void openSaveActivity() {
+        Intent intent = new Intent(this, SaveActivity.class);
+        startActivity(intent);
+    }
+
+    private void openEditActivity() {
+        Intent intent = new Intent(this, EditActivity.class);
+        startActivity(intent);
+    }
+
+    private void openMusicActivity() {
+        Intent intent = new Intent(this, MusicActivity.class);
+        startActivity(intent);
+    }
+
+    private void openVideoActivity() {
+        Intent intent = new Intent(this, VideoActivity.class);
+        startActivity(intent);
     }
 }
